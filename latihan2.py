646a25e0d0fe73650e088430f21bb4120d58b37c
azwarpratamaa <56994174+azwarpratamaa@users.noreply.github.com>
2019-11-15 22:29:31 +0700

    Update latihan2.py

--- latihan2.py
+++ latihan2.py
@@ -1,9 +1,9 @@
-print (" Program Menampilkan Bilangan Terbesar Dari Bilangan n ")
+print ("Program Menampilkan Bilangan Terbesar Dari Bilangan n")
 
 n = 1
 max = 0
 while n != 0 :
     if n > max :
         max = n
-    n = int(input("Masukkan Bilangan : "))
-print ("Bilangan Terbesar adalah : ", max)
+    n = int(input("Masukkan Bilangan :"))
+print ("Bilangan Terbesar adalah :", max)
